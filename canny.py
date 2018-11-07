02bc91d273e2a35f7c4797d6e5d5c24c6e933e47
XuchengTang <haroldtangtxc@gmail.com>
2018-11-06 22:11:36 -0500

    Finished Prewitt Operator. Then continue on non-maxima suppression

--- canny.py
+++ canny.py
@@ -18,16 +18,33 @@ class CannyOperation():
                 dataList[y][x] = rawData.getpixel((x, y))        #format rawdata into a double list
 
         formatData = np.array(dataList)
-        print(formatData)
+        #print(formatData)
 
-        GResult = self.gFilter(formatData)
-        print(GResult)
+        gResult = self.gFilter(formatData, i , j)
+        #print(gResult)
+        gradient= self.prewittOp(gResult, i , j)
+        print(gradient)
+        pic = Image.fromarray(gradient)
+        pic.show()
 
+    # def conv( self, image, i, j , weight, boundary):
+    #     self.image = image
+    #     self.i = i
+    #     self.j = j
+    #     self.weight = weight
+    #     self.boundary = boundary
+    #     i_new = len(image[0][:]) - boundary
+    #     j_new = len(image[:,-1]) - boundary
+    #     new_Image = np.zeros((i,j), dtype = np.float)
+    #     for a in range(boundary, j_new):
+    #         for b in range(boundary, i_new):
+    #             new_Image[a,b] = np.sum(image[a-len(weight[0][:]): a+len(weight[0][:]) + 1, b-len(weight[0][:]):b+len(weight[0][:]) + 1] * weight)
+    #     return new_Image
 
-
-
-    def gFilter( self, list ):
-        self.list = list
+    def gFilter( self, image, i, j ):
+        self.list = image
+        self.i = i
+        self.j = j
         gaussianFilter = np.array(([ 1, 1, 2, 2, 2, 1, 1 ],
                                    [ 1, 2, 2, 4, 2, 2, 1 ],
                                    [ 2, 2, 4, 8, 4, 2, 2 ],
@@ -35,17 +52,42 @@ class CannyOperation():
                                    [ 2, 2, 4, 8, 4, 2, 2 ],
                                    [ 1, 2, 2, 4, 2, 2, 1 ],
                                    [ 1, 1, 2, 2, 2, 1, 1 ]))
-        i_new = len(list[ 0 ][ : ])-6
-        j_new = len(list[ :, -1 ])-6
-        #print(j_new, i_new)
-        new_image = np.zeros((j_new,i_new), dtype=np.int)
-        for a in range(j_new):
-            for b in range(i_new):
-                new_image[a,b] = np.sum(list[ a: a+7, b:b+7 ] * gaussianFilter)/140
-        return new_image
+        # gImage = self.conv(image, i, j, gaussianFilter, 3)
+        # normalImage = gImage/140.0
+        # return normalImage
+        i_gau = len(image[ 0 ][ : ]) - 3
+        j_gau = len(image[ :, -1 ]) - 3
+        #print(j_gau, i_gau)
+        gauImage = np.zeros((i, j), dtype=np.float)
+        for a in range(3,j_gau):
+            for b in range(3,i_gau):
+                gauImage[a,b] = np.sum(image[ a - 3: a + 4, b - 3:b + 4 ] * gaussianFilter) / 140.0
+        return gauImage
 
-    def prewitt( self, image ):
-        return 0
+    def prewittOp( self, image, i, j):
+        self.image = image
+        self.i = i
+        self.j = j
+        i_pre = len(image[ 0 ][ : ]) - 4
+        j_pre = len(image[ :, -1 ]) - 4
+        prewitt_X = np.array(([ -1, 0, 1 ], [ -1, 0, 1 ], [ -1, 0, 1 ]))
+        prewitt_Y = np.array(([ 1, 1, 1 ], [ 0, 0, 0 ], [ -1, -1, -1 ]))
+        image_X= np.zeros((i, j), dtype=np.float)
+        image_Y= np.zeros((i, j), dtype=np.float)
+        image_gradient = np.zeros((i, j), dtype=np.float)
+
+        for a in range(4,j_pre):
+            for b in range(4,i_pre):
+                image_X[a,b] = np.sum(image[ a-1 : a+2, b-1 : b+2 ] * prewitt_X )
+
+        for x in range(4,j_pre):
+            for y in range(4,i_pre):
+                image_Y[x,y] = np.sum(image[ x-1 : x+2, y-1 : y+2 ] * prewitt_Y )
+
+        for m in range(i):
+            for n in range(j):
+                image_gradient[m][n] =np.sqrt(np.square(image_X[m][n])+np.square(image_Y[m][n]))
+        return image_gradient
 
 Canny = CannyOperation()
-Canny.canny("Lena256.bmp")
+Canny.canny("zebra-crossing-1.bmp")
